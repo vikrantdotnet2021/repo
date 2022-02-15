d110178d380893196c54695afce7633404aa7310
vikrantdotnet2021 <95204513+vikrantdotnet2021@users.noreply.github.com>
2022-02-15 23:57:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// WAP to print Hello.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            //console is a class
+            //writeline is a method
+            Console.ReadLine();
+        }
+    }
+}
+
